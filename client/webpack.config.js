b6009aa81306a21629fd8ea8065a538aec60a4cd
JFleming963 <144638343+JFleming963@users.noreply.github.com>
2024-07-27 09:53:22 -0500

    page image and favicon are bundled and loaded

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
@@ -16,14 +17,81 @@ module.exports = () => {
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Super Cool App',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'sw.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'Super Cool App',
+        short_name: 'Cool App',
+        description: 'The coolest app in the world',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        start_url: '/',
+        fingerprints: false,
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+            filename: 'icon-[size].png',
+          },
+        ],
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: 'favicon.ico', to: 'favicon.ico' },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.html$/i,
+          loader: 'html-loader',
+          options: {
+            sources: {
+              list: [
+                {
+                  tag: 'img',
+                  attribute: 'src',
+                  type: 'src',
+                },
+                {
+                  tag: 'link',
+                  attribute: 'href',
+                  type: 'src',
+                },
+              ],
+            },
+          },
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        }
       ],
     },
   };
