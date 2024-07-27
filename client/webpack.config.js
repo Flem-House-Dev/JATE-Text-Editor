d58cb6a846acabb571821138d165e0a2e7fd4b9b
JFleming963 <144638343+JFleming963@users.noreply.github.com>
2024-07-27 13:01:49 -0500

    implemented indexedDB crud operations, and added service worker registration

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,6 +18,7 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
       publicPath: '/',
+      clean: true,
     },
     plugins: [
       new HtmlWebpackPlugin({
